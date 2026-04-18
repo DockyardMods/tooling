a3bd597d35be4e7b05fdb536cc67f3ffc174f6d9
Thomas <77514919+TB516@users.noreply.github.com>
2026-04-18 05:59:55 -0400

    remove old unused file/function

--- src/core/paths.ts
+++
@@ -1,7 +0,0 @@
-export const requireArg = (value: string | undefined, flagName: string): string => {
-  if (!value) {
-    throw new Error(`Missing required argument: ${flagName}`);
-  }
-
-  return value;
-};
